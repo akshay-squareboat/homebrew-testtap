397c2bc40ecb40799982a0e052f4d5b8d1420cc8
Akshay Kathpal <akshay.kathpal@squareboat.com>
2020-11-19 13:36:58 +0530

    add Formula

--- test-cli-brew.rb
+++ test-cli-brew.rb
@@ -5,6 +5,8 @@ class TestCliBrew < Formula
   sha256 "aa1d42ae878e1adf77ff402e7a2c62b528a48327ca435c8bf37af8edc6b173aa"
   license "MIT"
 
+  depends_on "node"
+
   def install   
     bin.install "test-cli-brew" 
   end
