3f83fe80ba9ddfdbbdf91bc12f9ac80cc5180444
Akshay Kathpal <akshay.kathpal@squareboat.com>
2020-11-19 07:00:03 +0530

    add formula

---
+++ test-cli-brew.rb
@@ -0,0 +1,12 @@
+class TestCliBrew < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/akshay-squareboat/test-cli-brew/archive/v1.0.0.tar.gz"
+  sha256 "aa1d42ae878e1adf77ff402e7a2c62b528a48327ca435c8bf37af8edc6b173aa"
+  license "MIT"
+
+  def install   
+    bin.install "test-cli-brew" 
+  end
+  
+end
